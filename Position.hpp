c5e9c1b9e53f828008a0cc9255465340056f753c
Kevin Garner <kg.dev@protonmail.com>
2020-06-22 20:18:17 -0500

    Added Position_t struct and Actor class

---
+++ Position.hpp
@@ -0,0 +1,16 @@
+/*
+Position structure
+Written by Kevin Garner. kg.dev@protonmail.com
+Code repo located at: https://github.com/KevDev13/ASCII_Dungeon
+*/
+
+#pragma once
+
+namespace AsciiDungeon
+{
+	struct Position_t
+	{
+		int x;
+		int y;
+	};
+}
