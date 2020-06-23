2e2a23fa88c147bbcdefe6a5a9811a3e268285a4
Kevin Garner <kg.dev@protonmail.com>
2020-06-22 19:24:25 -0500

    moved asciidungeon.cpp to src folder and update project settings

--- src/asciidungeon.cpp
+++ src/asciidungeon.cpp
@@ -9,7 +9,7 @@ Written by Kevin Garner. kg.dev@protonmail.com
 Code repo located at: https://github.com/KevDev13/ASCII_Dungeon
 */
 
-#include "include/libtcod.h"
+#include "libtcod.h"
 
 int main()
 {
