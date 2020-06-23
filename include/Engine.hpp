dca78ae141f23a68e26aaff4739fce38f9d75bf9
Kevin Garner <kg.dev@protonmail.com>
2020-06-22 19:55:39 -0500

    comments changes only

--- include/Engine.hpp
+++ include/Engine.hpp
@@ -2,7 +2,6 @@
 Main game engine
 
 Written by Kevin Garner. kg.dev@protonmail.com
-
 Code repo located at: https://github.com/KevDev13/ASCII_Dungeon
 */
 
--- src/asciidungeon.cpp
+++ src/asciidungeon.cpp
@@ -1,11 +1,9 @@
 /*
-
 ASCII Dungeon
 
 A simple dungeon crawler written in libtcod.
 
 Written by Kevin Garner. kg.dev@protonmail.com
-
 Code repo located at: https://github.com/KevDev13/ASCII_Dungeon
 */
 
