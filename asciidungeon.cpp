0a2d8d851cd090a68314d66b8f349e3d950e0e4c
Kevin Garner <kg.dev@protonmail.com>
2020-06-22 19:15:35 -0500

    added main cpp file

---
+++ asciidungeon.cpp
@@ -0,0 +1,17 @@
+/*
+
+ASCII Dungeon
+
+A simple dungeon crawler written in libtcod.
+
+Written by Kevin Garner. kg.dev@protonmail.com
+
+Code repo located at: https://github.com/KevDev13/ASCII_Dungeon
+*/
+
+#include "include/libtcod.h"
+
+int main()
+{
+	return 0;
+}
