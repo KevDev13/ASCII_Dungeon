adeaa921a30aa5d99146cff08c7465a465d9ab6b
Kevin Garner <kg.dev@protonmail.com>
2020-06-28 07:18:00 -0500

    Added Player class

---
+++ src/Player.cpp
@@ -0,0 +1,13 @@
+/*
+Player class.
+
+Written by Kevin Garner. kg.dev@protonmail.com
+Code repo located at: https://github.com/KevDev13/ASCII_Dungeon
+*/
+
+#include "Player.hpp"
+
+namespace AsciiDungeon
+{
+
+}
