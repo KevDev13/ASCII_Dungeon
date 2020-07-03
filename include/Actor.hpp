6960656597252a63190a7724e7c2e887a6f23819
Kevin Garner <kg.dev@protonmail.com>
2020-07-03 12:39:55 -0500

    Renamed Position to Vector2D

--- include/Actor.hpp
+++ include/Actor.hpp
@@ -16,24 +16,24 @@ namespace AsciiDungeon
 	{
 	public:
 		Actor();
-		Actor(Position_t pos, char dispCh, int maxHealth);
+		Actor(Vector2D_t pos, char dispCh, int maxHealth);
 		~Actor();
 
-		void SetPosition(Position_t pos, bool checkBoundary = true);
+		void SetPosition(Vector2D_t pos, bool checkBoundary = true);
 		inline void SetDisplayCharacter(char ch) { m_displayCharacter = ch; }
 		void UpdateHealth(int hp);
 		void SetHealth(int hp);
 		inline void UpdateMaxHealth(int hp) { m_maxHealth += hp; }
 		inline void SetMaxHealth(int hp) { m_maxHealth = hp; }
 
-		void Move(Position_t pos);
+		void Move(Vector2D_t pos);
 		void Move(int x, int y);
 		void MoveUp();
 		void MoveDown();
 		void MoveLeft();
 		void MoveRight();
 
-		inline Position_t GetPosition() const { return m_currentPosition; }
+		inline Vector2D_t GetPosition() const { return m_currentPosition; }
 		inline char GetDisplayCharacter() const { return m_displayCharacter; }
 		inline int GetCurrentHealth() const { return m_currentHealth; }
 		inline int GetMaxHealth() const { return m_maxHealth; }
@@ -41,15 +41,15 @@ namespace AsciiDungeon
 		inline bool IsDead() const { return m_currentHealth <= 0; }
 
 	protected:
-		Position_t m_currentPosition;
+		Vector2D_t m_currentPosition;
 		char m_displayCharacter;
 
 		int m_maxHealth;
 		int m_currentHealth;
 
-		bool CheckMoveBoundary(Position_t newPos);
+		bool CheckMoveBoundary(Vector2D_t newPos);
 	};
 
-	extern const Position_t MAP_UPPER_LEFT_CORNER;
-	extern const Position_t MAP_LOWER_RIGHT_CORNER;
+	extern const Vector2D_t MAP_UPPER_LEFT_CORNER;
+	extern const Vector2D_t MAP_LOWER_RIGHT_CORNER;
 }
