8b7dc361caf152a87927308be4c1852efa53b304
Kevin Garner <kg.dev@protonmail.com>
2020-07-17 14:10:26 -0500

    comments only changes

--- include/MovementComponent.hpp
+++ include/MovementComponent.hpp
@@ -1,3 +1,10 @@
+/*
+MovementComponent structure
+
+Written by Kevin Garner. kg.dev@protonmail.com
+Code repo located at: https://github.com/KevDev13/ASCII_Dungeon
+*/
+
 #pragma once
 
 #include "Vector2D.hpp"
--- include/Vector2D.hpp
+++ include/Vector2D.hpp
@@ -1,5 +1,6 @@
 /*
-Position structure
+Vector2D type structure
+
 Written by Kevin Garner. kg.dev@protonmail.com
 Code repo located at: https://github.com/KevDev13/ASCII_Dungeon
 */
