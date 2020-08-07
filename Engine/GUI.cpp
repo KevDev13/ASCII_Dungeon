e20b8300c0a697f9987ab32097a7b1486f586524
Kevin Garner <kg.dev@protonmail.com>
2020-08-06 20:16:37 -0500

    added GUI class

---
+++ Engine/GUI.cpp
@@ -0,0 +1,13 @@
+/*
+GUI class
+
+Written by Kevin Garner. kg.dev@protonmail.com
+Code repo located at: https://github.com/KevDev13/ASCII_Dungeon
+*/
+
+#include "GUI.hpp"
+
+namespace gage
+{
+
+}
---
+++ Engine/GUI.hpp
@@ -0,0 +1,16 @@
+/*
+GUI class
+
+Written by Kevin Garner. kg.dev@protonmail.com
+Code repo located at: https://github.com/KevDev13/ASCII_Dungeon
+*/
+
+#pragma once
+
+namespace gage
+{
+	class GUI
+	{
+
+	};
+}
