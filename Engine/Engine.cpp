67c3be6460cc4bf0bb9782a247b546744d3e5c11
Kevin Garner <kg.dev@protonmail.com>
2020-08-07 15:41:36 -0500

    added another TODO

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -106,6 +106,7 @@ namespace gage
 			{
 				case State::PLAYING:
 
+					// TODO: have a method that will process entities & systems? might be easier, so can only call it if needed
 					m_inputHandler->HandlePlayerInput(m_registry, m_playerEntity, m_currentState);
 					// TODO: handle AI here
 					m_movementHandler->ProcessMovement(m_registry);
