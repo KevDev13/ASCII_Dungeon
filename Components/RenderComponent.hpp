11ac2200dca116fff9323239515f393b15e36ac8
Kevin Garner <kg.dev@protonmail.com>
2020-09-09 15:57:35 -0500

    added TODO

--- Components/RenderComponent.hpp
+++ Components/RenderComponent.hpp
@@ -16,7 +16,7 @@ namespace gage
 
 	struct RenderComponent
 	{
-		TCODColor backgroundColor;
+		TCODColor backgroundColor;	// TODO: can likely delete this
 		TCODColor foregroundColor;
 		char displayCharacter;
 
