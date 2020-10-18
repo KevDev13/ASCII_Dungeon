b038f5276e15bf753f561f63013581810e531df5
Kevin Garner <kg.dev@protonmail.com>
2020-10-18 13:38:42 -0500

    typo in comments

--- Engine/KageException.hpp
+++ Engine/KageException.hpp
@@ -1,5 +1,5 @@
 /*
-Custom exception class for use in GAGE.
+Custom exception class for use in KAGE.
 
 Written by Kevin Garner. kg.dev@protonmail.com
 Code repo located at: https://github.com/KevDev13/ASCII_Dungeon
